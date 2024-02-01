3486c31f2cdef4e22934fea44bcec40150ebc7e8
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-02-01 12:45:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function printfLength(url) {
+	let n =url.length;
+	alert("the length of the Url is :" n);
+}
+printfLength("https://course.acciojob.com/journey");
